6ce20cbe9d1558bb6feaf78011a9f71e30c6f9cd
rohith illuri <rohith.illuri@gmail.com>
2021-03-20 12:32:52 +0530

    op

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';  
+import "./Login.css";
+
+function Login() {
+    return (
+        <div className="login"> 
+            <img
+             src="https://image.jimcdn.com/app/cms/image/transf/none/path/s680f3df46a487bb1/image/i35250e2b8b88daa7/version/1532172546/image.jpg" 
+             alt=""
+             />      
+            <a>LOGIN WITH SPOTIFY</a>  
+
+        </div>
+    )
+}
+
+export default Login
